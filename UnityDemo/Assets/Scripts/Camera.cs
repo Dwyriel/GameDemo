294647c3abf361295a67c4581018daefddf88bdc
Dwyriel <dwyriel333@hotmail.com>
2023-02-12 16:24:17 -0300

    Camera movement

---
+++ UnityDemo/Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraTurretAnchor;
+    [SerializeField] private GameObject cameraBarrelAnchor;
+    [SerializeField] private GameObject aimAtAnchor;
+    [SerializeField] private float positionReductionWhenLookingUp = 2.65f;
+    [SerializeField] private float positionReductionWhenLookingDown = 3f;
+
+    void Update()
+    {
+        var turretAnchorPos = cameraTurretAnchor.transform.position;
+        var barrelAnchorPos = cameraBarrelAnchor.transform.position;
+        var y = barrelAnchorPos.y - turretAnchorPos.y;
+        y = turretAnchorPos.y + y / (y < 0 ? positionReductionWhenLookingUp : positionReductionWhenLookingDown);
+        transform.position = new Vector3(turretAnchorPos.x, y, turretAnchorPos.z);
+        transform.LookAt(aimAtAnchor.transform.position);
+    }
+}
