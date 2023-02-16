b7778798ee3aad342e215129804a799ab5c335a4
Dwyriel <dwyriel333@hotmail.com>
2023-02-16 06:47:51 -0300

    Better Enemy spawn, Enemy const values

--- UnityDemo/Assets/Scripts/ConstValuesAndUtility.cs
+++ UnityDemo/Assets/Scripts/ConstValuesAndUtility.cs
@@ -4,10 +4,6 @@ public static class ConstValuesAndUtility
 {
     public const string IpAddress = "127.0.0.1";
     public const int Port = 7030;
-    public const float DelayBeforeLoadingGameScene = 2f;
-    public const float DelayBeforeLoadingIdleScene = 5f;
-    public const int NumberOfEnemiesToSpawn = 3;
-    public const float MaxRoundTime = 120f;
 
     #region tags
 
@@ -16,6 +12,24 @@ public static class ConstValuesAndUtility
     public const string BulletTag = "Bullet";
     public const string ScenarioTag = "Scenario";
     public const string MapCenterPointTag = "MapCenterPoint";
+
+    #endregion
+
+    #region Round
+
+    public const float DelayBeforeLoadingGameScene = 2f;
+    public const float DelayBeforeLoadingIdleScene = 5f;
+    public const int NumberOfEnemiesToSpawn = 3;
+    public const float MaxRoundTime = 120f;
+
+    #endregion
+
+    #region Enemies
+
+    public const float MaxDistanceToMidAllowed = 300;
+    public const float RandomMoveTargetRange = 50f;
+    public const float MovingUnitsPerSecond = 15;
+    public const float RotationAnglePerSecond = 20f;
 
     #endregion
 
