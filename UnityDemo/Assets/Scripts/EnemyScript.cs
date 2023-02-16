5a37dcbf8c3df08fa48dd7d4bacbbf7819c61e97
Dwyriel <dwyriel333@hotmail.com>
2023-02-16 08:22:32 -0300

    EnemyPrefab Change

--- UnityDemo/Assets/Scripts/EnemyScript.cs
+++ UnityDemo/Assets/Scripts/EnemyScript.cs
@@ -15,16 +15,39 @@ public class EnemyScript : MonoBehaviour
     private void Start()
     {
         ConstValuesAndUtility.AddTag(transform, ConstValuesAndUtility.EnemyTag);
+        var rand = Random.Range(0, 3);
+        switch (rand)
+        {
+            case 0:
+                gameObject.AddComponent<EnemyHorizontalScript>();
+                break;
+            case 1:
+                gameObject.AddComponent<EnemyCircularScript>();
+                break;
+            case 2:
+                gameObject.AddComponent<EnemySinusWaveScript>();
+                break;
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider colliderObject)
     {
-        if (!other.CompareTag(ConstValuesAndUtility.BulletTag))
+        CollisionWithTag(colliderObject, ConstValuesAndUtility.BulletTag);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CollisionWithTag(collision.collider, ConstValuesAndUtility.EnemyTag);
+    }
+
+    private void CollisionWithTag(Collider collisionCollider, string possibleTag)
+    {
+        if(!collisionCollider.CompareTag(possibleTag))
             return;
         if(_gotHit)
             return;
         _gotHit = true;
         EnemyDestroyedEvent?.Invoke();
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
---
+++ UnityDemo/Assets/Scripts/EnemySinusWaveScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySinusWaveScript : MonoBehaviour
+{
+    private enum CurrentState
+    {
+        AwayFromTarget,
+        FinishRotating,
+        FlyUntilCloseToTarget,
+        Rotating
+    }
+    
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
